cb6e0449ac3d2b57ac7a19cde5f268dc29715e1a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2019-08-15 06:10:48 +0900

    Middleware

--- Chapter06/6.3/learning-express/app.js
+++ Chapter06/6.3/learning-express/app.js
@@ -16,8 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(function(req, res, next) {
-    console.log(req.url, '저도 미들웨어입니다');
-    next();
+  console.log(req.url, '저도 미들웨어입니다');
+  next();
 });
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,13 +25,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('secret code'));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'secret code',
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
+  resave: false,
+  saveUninitialized: false,
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
 }));
 app.use(flash());
 
@@ -40,18 +40,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
